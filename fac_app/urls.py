f0071b94ddff62486a6cd3fbc8b0ac533d4c756f
CAMARAPRO <camaracodeur.pro@gmail.com>
2023-10-31 12:53:03 -1200

    feat: add new customer

--- fac_app/urls.py
+++ fac_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from fac_app.views import *
 
 urlpatterns = [
-    path('',HomeView.as_view(), name='home')
+    path('',HomeView.as_view(), name='home'),
+    path('add-customer', AddCustomerView.as_view(), name='add-customer')
 ]
--- fac_app/views.py
+++ fac_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.views import View
 from .models import*
 
@@ -20,3 +21,36 @@ class HomeView(View):
     
     def post(self, request, *args, **kwags):
         return render(request, self.templates_name, self.context)
+    
+
+class AddCustomerView(View):
+    """  add new customer """
+
+    template_name = 'add_customer.html'
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+    
+    def post(self, request, *args, **kwargs):
+        data = {
+            'name': request.POST.get('name'),
+            'email': request.POST.get('email'),
+            'phone': request.POST.get('phone'),
+            'address': request.POST.get('address'),
+            'sex': request.POST.get('sex'),
+            'age': request.POST.get('age'),
+            'city': request.POST.get('city'),
+            'zip_code': request.POST.get('zip'),
+            'save_by': request.user
+        }
+        try:
+            created = Customer.objects.create(**data)
+
+            if created:
+                messages.success(request, f"Customer {request.POST.get('name')} created successfuly.")
+            else:
+                messages.error(request, "Sorry, please try again the sent data is corrupt")
+
+        except Exception as e:
+            messages.error(request, f"Sorry our system is detecting the following issues {e}.")
+
+        return render(request, self.template_name)
